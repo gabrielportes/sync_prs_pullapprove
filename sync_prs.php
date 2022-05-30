4018a7b9b275745401c36191b69d11881b414b12
Gabriel Portes <gabriel-portes@hotmail.com>
2022-05-30 18:31:17 -0300

    generalizando nome dos repositórios

--- sync_prs.php
+++ sync_prs.php
@@ -8,11 +8,25 @@
 const COOKIE = '';
 
 /**
+ * Esse token tem no Cookie e tem também no playload quando você sincroniza a PR via interface.
+ *
+ * @var string Token CSRF.
+ */
+const CSRF = 'csrfmiddlewaretoken=';
+
+/**
  * Repositório que você deseja sincronizar.
  *
  * @var string Repositório.
  */
-const REPO = 'cloud';
+const REPO = '';
+
+/**
+ * Url do Pull Approve com o repositório.
+ *
+ * @var string Url do Pull Approve.
+ */
+const PULLAPPROVE_REPO = 'https://pullapprove.com/' . REPO;
 
 print_r("SCRIPT PARA SINCRONIZAR TODAS PRs ABERTAS NO PULLAPPROVE\n");
 syncPrs(getPrs());
@@ -25,6 +39,8 @@ syncPrs(getPrs());
 function getPrs(): array
 {
     $repo = REPO;
+    $repoParaRegex = str_replace('/', '\/', $repo);
+    $pullApproveRepo = PULLAPPROVE_REPO;
     $cookie = COOKIE;
     $pagina = 1;
     $ultimaPagina = 0;
@@ -32,7 +48,7 @@ function getPrs(): array
     do {
         print_r("\n\nIdentificando as PRs da página {$pagina}:\n");
         exec(
-            "curl 'https://pullapprove.com/Superlogica/{$repo}/?page={$pagina}' \
+            "curl '{$pullApproveRepo}/?page={$pagina}' \
             -H 'Connection: keep-alive' \
             -H 'Pragma: no-cache' \
             -H 'Cache-Control: no-cache' \
@@ -55,16 +71,16 @@ function getPrs(): array
         foreach ($output as $linha) {
             if (
                 $pagina === 1
-                && strpos($linha, "/Superlogica/{$repo}/?page=") !== false
+                && strpos($linha, "/{$repo}/?page=") !== false
             ) {
-                preg_match("/\/Superlogica\/{$repo}\/\?page=(\d+)/", $linha, $matches);
+                preg_match("/\/{$repoParaRegex}\/\?page=(\d+)/", $linha, $matches);
                 if ($matches[1] > $ultimaPagina) {
                     $ultimaPagina = $matches[1];
                 }
             }
 
-            if (strpos($linha, "/Superlogica/{$repo}/pull-request/") !== false) {
-                preg_match("/\/Superlogica\/{$repo}\/pull-request\/(\d+)\//", $linha, $matches);
+            if (strpos($linha, "/{$repo}/pull-request/") !== false) {
+                preg_match("/\/{$repoParaRegex}\/pull-request\/(\d+)\//", $linha, $matches);
                 $prs[$matches[1]] = $matches[1];
             }
         }
@@ -85,12 +101,12 @@ function getPrs(): array
 function syncPrs(array $prs)
 {
     print_r("\n\n\nSINCRONIZANDO AS PRs\n\n\n");
-    $repo = REPO;
+    $pullApproveRepo = PULLAPPROVE_REPO;
     $cookie = COOKIE;
-    print_r($prs);die;
+    $dataRaw = CSRF;
     foreach ($prs as $pr) {
         print_r("PR {$pr}:");
-        exec("curl 'https://pullapprove.com/Superlogica/{$repo}/pull-request/{$pr}/sync/' \
+        exec("curl '{$pullApproveRepo}/pull-request/{$pr}/sync/' \
         -H 'Connection: keep-alive' \
         -H 'Pragma: no-cache' \
         -H 'Cache-Control: no-cache' \
@@ -106,10 +122,10 @@ function syncPrs(array $prs)
         -H 'Sec-Fetch-Mode: navigate' \
         -H 'Sec-Fetch-User: ?1' \
         -H 'Sec-Fetch-Dest: document' \
-        -H 'Referer: https://pullapprove.com/Superlogica/{$repo}/pull-request/{$pr}/' \
+        -H 'Referer: {$pullApproveRepo}/pull-request/{$pr}/' \
         -H 'Accept-Language: pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7' \
         -H 'Cookie: {$cookie}' \
-        --data-raw 'csrfmiddlewaretoken=8hBI4rUK9Q8HvwnHgvCVxEH2hO3X2PQ7' \
+        --data-raw '{$dataRaw}' \
         --compressed");
         print_r("\n\n");
     }
